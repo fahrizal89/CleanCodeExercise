a20547fc6682ae1c0af0da09facf75efca258927
fahrizal89 <fahrizalfms@gmail.com>
2025-08-01 10:54:02 +0700

    trigger onAmountUpdated and onSubmitClicked

--- app/src/main/java/id/sample/cleancodeexercise/MainActivity.kt
+++ app/src/main/java/id/sample/cleancodeexercise/MainActivity.kt
@@ -1,13 +1,11 @@
 package id.sample.cleancodeexercise
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
@@ -18,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var amountInput: EditText
     private lateinit var taxText: TextView
     private lateinit var totalText: TextView
+    private lateinit var submitBtn: Button
     private var balance = 100000.0
     private var groupingUsed:Boolean=false
 
@@ -27,8 +26,11 @@ class MainActivity : AppCompatActivity() {
 
         remainingBalanceText = findViewById(R.id.remainingBalanceText)
         amountInput = findViewById(R.id.amountInput)
+        amountInput.setOnKeyListener { view, i, keyEvent ->  onAmountUpdated(); false }
         taxText = findViewById(R.id.taxText)
         totalText = findViewById(R.id.totalText)
+        submitBtn = findViewById(R.id.submitBtn)
+        submitBtn.setOnClickListener { onSubmitClicked() }
 
         remainingBalanceText.text = "Rp 100.000,00"
     }
